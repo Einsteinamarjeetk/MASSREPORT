bdd517757104c544f2ebb40f971baa4cd364d9ae
Einsteinamarjeetk <156225038+Einsteinamarjeetk@users.noreply.github.com>
2024-06-09 12:50:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Bot (Client):
 
     def __init__(self):
         super().__init__(
-            name="ReportBot",
+            name="EinsteinReportBot",
             in_memory=True,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
